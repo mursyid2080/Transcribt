5789377c221ed31c80780fe6dd471e10c607a783
mursyid2080 <malekmursyid@gmail.com>
2024-04-21 09:41:37 -0400

    user#4

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mursyid/testdjango/myenv"
+set -gx VIRTUAL_ENV "/home/mursyid/Transcribt/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
